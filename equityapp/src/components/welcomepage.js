6ec45e947c507eb2d53901bb47021845592d64c5
mnaishla <59778041+mnaishla@users.noreply.github.com>
2024-04-05 13:44:24 -0700

    page shows in browser

---
+++ equityapp/src/components/welcomepage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function WelcomePage() { // Capitalized function name
+    // const navigate = useNavigate();
+    return (
+        <h1>Homepage</h1>
+    );
+}
+
+export default WelcomePage; // Capitalized export name
