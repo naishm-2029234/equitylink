dae1ff27fe0f0e0be141fdae07363137f89da774
amberc15 <ambercarbajal33@gmail.com>
2024-04-09 14:18:40 -0700

    insights page

--- equityapp/src/components/insights.js
+++ equityapp/src/components/insights.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import '../AboutPage.css';
+import banner from '../img/resourcebanner.jpg';
 
 function Insights() {
   return (
     <div>
-<p> insights</p>
+      <div className="banner">
+        <img src={banner} alt="Banner Image" />
+        <div className="banner-text">
+          <h1>Data Insights</h1>
+        </div>
+      </div>
+
+      <div className="Insights">
+        <p>Welcome to the Insights page, your go-to destination for data-driven insights that can inform and enhance your program-building efforts in undersupported communities. This section will answer key questions about demographics and other relevant information, empowering you with the knowledge needed to make informed decisions and maximize the impact of your initiatives. Source 2021 US Census </p>
+      </div>
+
+      <div className="Demographic Insights">
+        <h2>Demographic Insights</h2>
+      </div>
     </div>
   );
 }
 
 export default Insights;
+
+
