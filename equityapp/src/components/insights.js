499b50d835ea022e15134968a818012cdfe67d0e
amberc15 <ambercarbajal33@gmail.com>
2024-04-12 06:43:32 -0700

    data insights formatting fixed

--- equityapp/src/components/insights.js
+++ equityapp/src/components/insights.js
@@ -8,15 +8,15 @@ function Insights() {
       <div className="banner">
         <img src={banner} alt="Banner Image" />
         <div className="banner-text">
-          <h1>Data Insights</h1>
+          <h1>DATA INSIGHTS</h1>
         </div>
       </div>
 
-      <div className="Insights">
+      <div className="purpose">
         <p>Welcome to the Insights page, your go-to destination for data-driven insights that can inform and enhance your program-building efforts in undersupported communities. This section will answer key questions about demographics and other relevant information, empowering you with the knowledge needed to make informed decisions and maximize the impact of your initiatives. Source 2021 US Census </p>
       </div>
 
-      <div className="Demographic Insights">
+      <div className="about-us">
         <h2>Demographic Insights</h2>
       </div>
     </div>
