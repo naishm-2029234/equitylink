abfd4daf88e9186a90f91c427d6475d87474ae7d
YuehChiLin <43730612+YuehChiLin@users.noreply.github.com>
2024-04-30 01:24:43 -0700

    Add files via upload

--- equityapp/src/App.js
+++ equityapp/src/App.js
@@ -3,6 +3,15 @@ import { BrowserRouter as Router, Routes, Route, Switch } from 'react-router-dom
 import Navigation from './components/navbar';
 import FindingResources from './components/findresources';
 import Budgeting from './components/funding';
+
+import Invest from './components/Invest';
+import Assess from './components/Assess';
+import Offer from './components/Offer';
+import Collaborate from './components/Collaborate';
+import Support from './components/Support';
+import Provide from './components/Provide';
+
+
 import PractitionerToolkit from './components/practitioner';
 import AboutUs from './components/AboutUs';
 import Homepage from './components/homepage';
@@ -18,6 +27,12 @@ const App = () => {
                     {/* <Route path="/findresources" element={<FindingResources />} /> */}
                     <Route path="/" element={<Homepage />} />
                     <Route path="/funding" element={<Budgeting />} />
+                    <Route path="/Assess" element={<Assess />} />
+                    <Route path="/Invest" element={<Invest />} />
+                    <Route path="/Offer" element={<Offer />} />
+                    <Route path="/Collaborate" element={<Collaborate />} />
+                    <Route path="/Support" element={<Support />} />
+                    <Route path="/Provide" element={<Provide />} />
                     <Route path="/practitioner" element={<PractitionerToolkit />} />
                     <Route path="/AboutUs" element={<AboutUs />} />
                     <Route path="/homepage" element={<Homepage />} />
