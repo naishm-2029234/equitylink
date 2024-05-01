301c9afe65cf67cbd8537e0fb0eda973e1bf17db
Jiyeong Kang <jiyeongkang@Jiyeongui-MacBookPro-2.local>
2024-05-01 12:05:56 -0700

    added footer banner, edited texts, and deleted budgeting calculator section from the budgeting page.

---
+++ equityapp/src/components/FooterBanner.js
@@ -0,0 +1,12 @@
+// FooterBanner.js
+import React from 'react';
+
+const FooterBanner = () => {
+    return (
+        <div className="footer-banner">
+            <p>This website is an ongoing project sponsored by Katya. For inquiries, contact Yekaterina Yefimova at <a href="yekaty@uw.edu">yekaty@uw.edu</a>.</p>
+        </div>
+    );
+};
+
+export default FooterBanner;
